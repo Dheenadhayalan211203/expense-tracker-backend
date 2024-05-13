0962192126587c038f49cf2c83c4cff4177f0965
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-05-13 12:34:01 +0530

    full update

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const multer = require("multer");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 const PORT = 3000;
 require("dotenv").config();
 
@@ -16,36 +14,16 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
-// Define the User1 schema
-const user1Schema = new mongoose.Schema({
-  title: String,
-  amount: Number,
-});
-
-// Create the User1 model
-const User1 = mongoose.model("user1", user1Schema);
-
 // Create Expense schema
 const expenseSchema = new mongoose.Schema({
   title: String,
   amount: Number,
   date: String,
-  imageData: String, 
 });
 
 const Expense = mongoose.model("Expense", expenseSchema);
 
-// Multer storage configuration
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
+app.use(bodyParser.json());
 
 // API to get all expenses
 app.get("/api/expenses", async (req, res) => {
@@ -58,12 +36,11 @@ app.get("/api/expenses", async (req, res) => {
 });
 
 // API to create a new expense
-app.post("/api/expenses", upload.single("image"), async (req, res) => {
+app.post("/api/expenses", async (req, res) => {
   const expense = new Expense({
     title: req.body.title,
     amount: req.body.amount,
     date: req.body.date,
-    imageData: req.file.path, 
   });
 
   try {
@@ -84,7 +61,6 @@ app.put("/api/expenses/:id", async (req, res) => {
     expense.title = req.body.title;
     expense.amount = req.body.amount;
     expense.date = req.body.date;
-    expense.imageData = req.body.imageData; 
     const updatedExpense = await expense.save();
     res.json(updatedExpense);
   } catch (err) {
@@ -104,24 +80,6 @@ app.delete("/api/expenses/:id", async (req, res) => {
     res.json({ message: "Expense deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
-  }
-});
-
-// Route to add a product to the "user1" collection
-app.post("/api/user1", async (req, res) => {
-  const { title, amount } = req.body;
-
-  // Create a new User1 document
-  const product = new User1({
-    title: title,
-    amount: amount,
-  });
-
-  try {
-    const newProduct = await product.save();
-    res.status(201).json(newProduct);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
   }
 });
 
